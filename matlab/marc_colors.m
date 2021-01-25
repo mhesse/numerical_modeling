40cc92fceb7bdea06f24cf23048cdc6379387fae
Marc Hesse <mhesse@jsg.utexas.edu>
2021-01-25 10:09:04 -0600

    Add files via upload

---
+++ matlab/marc_colors.m
@@ -0,0 +1,9 @@
+function [col] = marc_colors()
+% Colors Marc likes - or at least used to like
+col.red    = [190  30  45]/255; 
+col.blue   = [ 39 170 225]/255; 
+col.green  = [  0 166  81]/255;
+col.orange = [247 148  30]/255;
+col.purple = [102  45 145]/255;
+col.brown  = [117  76  36]/255;
+col.tan    = [199 178 153]/255;
